77de1df94187b6b10c2d0ffa0b1786ede232d155
Johannes Ström <johannes.strom@svenskaspel.se>
2024-10-02 10:26:51 +0200

    new commands and more controll over them, also raid detection

--- readme.md
+++ readme.md
@@ -6,22 +6,24 @@ Simply make sure FFMPEG and python is installed then run
 >pip install -r requirmenets.txt
 
 after that make sure to edit the `config.json` with a bot access token,\
-if ur using ur own twitch account as the bot you dont need to do anything else,\
+if ur using ur own twitch account as the bot you dont need to do anything else, The bot will automatically join your twitch chat\
 however if ur using an actuall bot account you must also specify the channels you want it to join in the `config.json`
 
 config Example
+```json
+ {
+    "twitchKey": "TOKEN12312331",
+    "prefix": "!",
+    "targetChannels": ['dougdoug'] // will join the doug doug twitch chat
+  }
 ```
-{
-  "twitchKey": "TOKEN12312331",
-  "prefix": "!",
-  "targetChannels": ['dougdoug'] <-- will join the doug doug twitch chat
-}
-```
+
+
 To ensure you have the right channel for ur purpose you can simply look att the URL of the twitch streamer page (ex. [twitch.tv/dougdoug](https://www.twitch.tv/dougdoug))
 
 ### Running the bot
 you can choose to run it with or without the control panel.\
-To run with control panel do:
+To run with control panel:
 >python main.py
 
 To run without control panel:
--- ttsBot.py
+++ ttsBot.py
@@ -9,13 +9,16 @@ class twitchBot(commands.Bot):
   #todo: add a raid announcment message
 
     def __init__(self, token, prefix, targetChannels):
+        self.settings = {}
+        with open('settings.json') as f:
+            self.settings = json.load(f)
         self.active = True # type: ignore
         self.prefix = prefix
         self.sayName = False
         # Initialise our Bot with our access token, prefix and a list of channels to join on boot...
         # prefix can be a callable, which returns a list of strings or a string...
         # initial_channels can also be a callable which returns a list of strings...
-        super().__init__(token=token, prefix=prefix, initial_channels=targetChannels)
+        super().__init__(token=token, prefix=prefix, initial_channels=targetChannels, case_insensitive=True)
 
     async def event_ready(self):
         # Notify us when everything is ready!
@@ -32,39 +35,61 @@ class twitchBot(commands.Bot):
       tts.write_to_fp(mp3_fp)
       mp3_fp.seek(0)
       play(AudioSegment.from_mp3(mp3_fp))
+    
+    def commandActive(self, command):
+      return self.settings.activeCommands[command]
+    
+    def getMessage(self, command):
+      return self.settings.messages[command]
+    
+
+    async def event_raided(self, channel, user, raid_count):
+        print(f'Raid detected! {user} raided {channel} with {raid_count} viewers!')
+        if (self.active and self.commandActive('raid')):
+          await channel.send(self.getMessage('raid').format(n=raid_count, u=user))
 
     @commands.command()
     async def tts(self, ctx: commands.Context): # make tts
-        if (self.active):
+        if (self.active and self.commandActive('tts')):
           message = ctx.message.content[len(self.prefix)+3:]
           self.newTTS(f'{ctx.author.name} says {message}' if self.sayName else message)
 
     @commands.command()
     async def about(self, ctx: commands.Context): #tell us about the channel
-        aboutText = "test"
-        if (self.active):
-          ctx.message.channel.send(aboutText)
+        if (self.active and self.commandActive('about')):
+          ctx.message.channel.send(self.getMessage('about'))
 
     @commands.command()
     async def help(self, ctx: commands.Context): #list commands and their use
-        helpText = "test"
-        if (self.active):
-          ctx.message.channel.send(helpText)
+        if (self.active and self.commandActive('help')):
+          ctx.message.channel.send(self.getMessage('help'))
 
     @commands.command()
     async def shoutout(self, ctx: commands.Context): #shoutout a user
-      pass
+      if (self.active and self.commandActive('shoutout')):
+        message = ctx.message.content[len(self.prefix)+8:]
+        ctx.message.channel.send(self.getMessage('shoutout').format(u=message))
 
     @commands.command()
     async def rules(self, ctx: commands.Context): #display rule number x or all rules/link to rules 
-      pass
+        if (self.active and self.commandActive('rules')):
+          ctx.message.channel.send(self.getMessage('rules'))
 
     @commands.command()
-    async def discord(self, ctx: commands.Context): #display the discord join link
+    async def discord(self, ctx: commands.Context): #display the discord message
+        if (self.active and self.commandActive('discord')):
+          ctx.message.channel.send(self.getMessage('discord'))
+
+    @commands.command()
+    async def soundboard(self, ctx: commands.Context): #soundboard plays sounds on stream
+      if (self.active and self.commandActive('soundboard')):
+        pass
       pass
 
     @commands.command()
     async def placeholder(self, ctx: commands.Context): #placeholder function to make more commands
+      if (self.active and self.commandActive('placeholder')):
+        pass
       pass
 
 if __name__ == "__main__":
